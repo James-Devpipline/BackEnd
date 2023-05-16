bd72e4ed98af426a4201022dc107227902b0e48a
james <james@devpipeline.com>
2023-05-16 13:58:39 -0600

    added notes

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -16,11 +16,11 @@ After reversing it  [89, 45, 78]
 sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
 
 def reverse_list(list):
-    needed_length = len(list) / 3
+    needed_length = len(list) // 3 # using floor devision returns an int while regular always returns a float
     print(needed_length)
-    chunk_1 = list[slice(0, int(needed_length))]
-    chunk_2 = list[slice(int(needed_length), int(needed_length*2))]
-    chunk_3 = list[slice(int(needed_length*2), int(needed_length*needed_length))]
+    chunk_1 = list[slice(0, needed_length)]
+    chunk_2 = list[slice(needed_length, needed_length*2)]
+    chunk_3 = list[slice(needed_length*2, needed_length*needed_length)]
     print('-----BEFORE REVERSE-----')
     print(chunk_1)
     print(chunk_2)
@@ -36,3 +36,6 @@ def reverse_list(list):
     print(chunk_3)
 
 reverse_list(sample_list)
+
+# sort()
+# sorted()
