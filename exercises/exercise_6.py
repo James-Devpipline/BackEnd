513c0d11b828c917fbdce990c67d95e65fae899f
james <james@devpipeline.com>
2023-06-20 13:26:10 -0600

    added 6th exercise

---
+++ exercises/exercise_6.py
@@ -0,0 +1,39 @@
+'''
+Challenge 1: Anagram Checker
+Write a function is_anagram(word1, word2) that takes two strings word1 and word2 as input and returns True if the words are anagrams (contain the same letters in a different order), and False otherwise.
+Example:
+Input: "listen" , "silent"
+Output: True
+Challenge 2: List Flattening
+Write a function flatten_list(lst) that takes a nested list lst as input and returns a new list that contains all the elements from the nested list in a flattened structure.
+Example:
+Input: [1, [2, [3, 4], 5], 6]
+Output: [1, 2, 3, 4, 5, 6] 
+'''
+
+def is_anagram(word1, word2): 
+    print(f'first word: {word1}')
+    print(f'second word: {word2}')
+
+    if sorted(word1) == sorted(word2):
+        return True
+    else:
+        return False
+
+print(is_anagram("listen", "silent"))
+print(is_anagram("test", "asdf"))
+
+print('\n-------------------------------------------\n')
+
+def flatten_list(lst):
+    working_list = []
+    for nested_list in lst:
+        for item in nested_list:
+            working_list.append(item)
+    return working_list
+
+test_list = [[1], [2, 3], [4, 5, 6, 7]]
+
+print(f'Before: {test_list}')
+print(f'After: {flatten_list(test_list)}')
+
