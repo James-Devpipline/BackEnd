dd670557d0708fb9c1a016ee060037f6a7eba4b0
james <james@devpipeline.com>
2023-06-27 16:08:42 -0600

    removed 5th notes folder as I will update the 5th assignments folder instead, I really hope this comment doesnt go over 50 words otherwise that would be really funny in my opinion. WORDS WORDS WORDS

--- notes/5-marsh-refactor/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .muscles_routes import muscles
-from .personal_records_routes import pr
-from .recorded_exercises_routes import rec
-from .exercise_types_routes import ex_type
-from .exercises_routes import ex
