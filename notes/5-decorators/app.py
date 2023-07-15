0c75b511c69fe98533e4e78d3beb0560fc4c9597
james <james@devpipeline.com>
2023-07-15 15:49:32 -0600

    further arg and kwargs notes

--- notes/5-decorators/app.py
+++ notes/5-decorators/app.py
@@ -24,32 +24,61 @@ Pass by Value vs Pass by Reference
 '''
 
 
+# from decorators import my_decorator
+
+
+# def my_func(lst):
+#     return lst.sort()
+
+
+# my_list = [1, 4, 5, 2, 8, 10, 3, -43]
+
+# #######
+# # PASS BY VALUE (passing in a value)
+# print(my_list)  # returns 1, 4, 5, 2, 8, 10, 3, -43]
+# new_lst = my_func(my_list)
+# print(new_lst)  # returns [-43, 1, 2, 3, 4, 5, 8, 10]
+# print(my_list)  # returns 1, 4, 5, 2, 8, 10, 3, -43]
+
+# #######
+# # PASS BY REFERENCE (passing in the reference of the variable (value) which then returns the same value but mutated)
+# print(my_list)  # returns 1, 4, 5, 2, 8, 10, 3, -43]
+# my_func(my_list)
+# print(my_list)  # returns [-43, 1, 2, 3, 4, 5, 8, 10]
+
+
+# @my_decorator
+# def say_hello(message):
+#     return message
+
+
+# print(say_hello("hello"))
+##########################
+# my_original_value = [8]
+
+
+# def my_function(a):
+#     a.append(5)
+#     return a
+#     # returns a reference to my_original_value, not returning a
+
+
+# my_new_val = my_function(my_original_value)
+
+# print(my_original_value is my_new_val)  # returns true because my new val is
+
+# my_new_val.append(9)
+
+# print(my_original_value)  # prints out my_new_val with the 9 due to pass by reference
+##########################
+
 from decorators import my_decorator
 
 
-def my_func(lst):
-    return lst.sort()
-
-
-my_list = [1, 4, 5, 2, 8, 10, 3, -43]
-
-#######
-# PASS BY VALUE (passing in a value)
-print(my_list)  # returns 1, 4, 5, 2, 8, 10, 3, -43]
-new_lst = my_func(my_list)
-print(new_lst)  # returns [-43, 1, 2, 3, 4, 5, 8, 10]
-print(my_list)  # returns 1, 4, 5, 2, 8, 10, 3, -43]
-
-#######
-# PASS BY REFERENCE (passing in the reference of the variable (value) which then returns the same value but mutated)
-print(my_list)  # returns 1, 4, 5, 2, 8, 10, 3, -43]
-my_func(my_list)
-print(my_list)  # returns [-43, 1, 2, 3, 4, 5, 8, 10]
-
-
 @my_decorator
-def say_hello(message):
-    return message
+def say_hello(message, message_kwarg):
+    return f'{message}\n{message_kwarg}'
 
 
-print(say_hello("hello"))
+print(say_hello)  # this returns its place in memory I believe
+print(say_hello("hello", message_kwarg="hello from kwargs"))
--- notes/5-decorators/decorators.py
+++ notes/5-decorators/decorators.py
@@ -1,12 +1,26 @@
 import functools
 
+# Decorators are functions that take in functions as their perameter and then expand on those functions or add functionalityß
+
 
 def my_decorator(func):
     @functools.wraps(func)
-    def decorator_wrapper(*args, **kwargs):
+    def decorator_wrapper(*args, **kwargs):  # * is like the spread operator in javascript. This gives a tuple of info of the spread out first perameter. ** changes it to a dictionary instead of a tuple. The argument name is the key and the passed in value is the value
         # some code goes here to do your magic things
+        print(args)
         new_message = f"{args[0].upper()}!!!!!!!!!!!!"
-        args = [new_message]
+        args = [new_message]  # due to tuples being immutable you have to destroy and replace args
+
+        print(kwargs)  # returns empty if no kwargs are passed in
+        kwargs['message_kwarg'] = f"{kwargs['message_kwarg'].upper()}!!!!!!!!!!!!"
         return func(*args, **kwargs)
 
     return decorator_wrapper
+
+
+# def function_name(a, b, c, d, e, f):
+
+#     pass
+
+
+# function_name(1, 2, 3, x=5, b=22)  # manually specified perams are the keyword arguments
