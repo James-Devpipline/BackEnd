004cc62831bf3a4f9ec2a0e2d2c58d38b68a421e
james <james@devpipeline.com>
2023-08-09 18:46:18 -0600

    added files

--- notes/8-hosting/entrypoint.sh
+++ notes/8-hosting/entrypoint.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-pipenv run gunicorn app:app
+pipenv run gunicorn app:app --config gunicorn_config.py app:app
 # first app is app.py, second is the app = init in app.py
+
+# docker run --name add-auth-model -e DATABASE_PRE="postgresql://" -e DATABASE_USER="james" -e DATABASE_ADDR="127.0.0.1" -e DATABASE_PORT="8432" -e DATABASE_NAME="marshmallow" -e DATABASE_PASS="12345678" -d -p 8000:8000 8-hosting:latest
